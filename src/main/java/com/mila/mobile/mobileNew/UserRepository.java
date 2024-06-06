a90c0cd37426fab9e9b83d9b67eb75c16aa4eddc
Mila Rose <milarose.dev@gmail.com>
2024-06-06 17:19:04 +0100

    Added random gen for userID

--- src/main/java/com/mila/mobile/mobileNew/UserRepository.java
+++ src/main/java/com/mila/mobile/mobileNew/UserRepository.java
@@ -4,6 +4,11 @@ import com.mila.mobile.mobileNew.io.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
+// -- query database  with CRUD--
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+
+
 }
--- src/main/java/com/mila/mobile/mobileNew/service/impl/UserServiceImpl.java
+++ src/main/java/com/mila/mobile/mobileNew/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.mila.mobile.mobileNew.service.impl;
 import com.mila.mobile.mobileNew.UserRepository;
 import com.mila.mobile.mobileNew.io.entity.UserEntity;
 import com.mila.mobile.mobileNew.service.UserService;
+import com.mila.mobile.mobileNew.shared.Utils;
 import com.mila.mobile.mobileNew.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,19 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    Utils utils;
     @Override
     public UserDto createUser(UserDto user) {
+
+        // -- check if email already exists before saving --
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("User record already exists");
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
-        userEntity.setEncryptedPassword("Test");
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setEncryptedPassword(publicUserId);
         userEntity.setUserId("TestUserID");
 
 
