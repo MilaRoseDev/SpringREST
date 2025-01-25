b81138fa651eb5ce5b34fa46332e72c34cfd9196
Mila Rose <milarose.dev@gmail.com>
2025-01-25 17:02:40 +0000

    Updated WebSecurity added SecurityConstants  and updated UserServiceImpl. - Added jjwt-api, jjwt-impl and jjwt-jackson

--- src/main/java/com/mila/mobile/mobileNew/service/impl/UserServiceImpl.java
+++ src/main/java/com/mila/mobile/mobileNew/service/impl/UserServiceImpl.java
@@ -7,10 +7,13 @@ import com.mila.mobile.mobileNew.shared.Utils;
 import com.mila.mobile.mobileNew.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -44,6 +47,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UserEntity userEntity = userRepository.findByEmail(username);
+
+        if(userEntity == null) throw new UsernameNotFoundException(username);
+
+
+        return new User(username, userEntity.getEncryptedPassword(), new ArrayList<>());
     }
 }
--- src/main/java/com/mila/mobile/mobileNew/webSecurity/WebSecurity.java
+++ src/main/java/com/mila/mobile/mobileNew/webSecurity/WebSecurity.java
@@ -1,18 +1,21 @@
+
 package com.mila.mobile.mobileNew.webSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurity {
+
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -23,14 +26,19 @@ public class WebSecurity {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+        // Configure HttpSecurity
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/users", HttpMethod.POST.name())) // Disable CSRF for specific endpoint
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/users", HttpMethod.POST.name())).permitAll()
+                .cors(AbstractHttpConfigurer::disable) // Customize or disable CORS
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
                         .anyRequest().authenticated()
                 );
+
         return http.build();
     }
 
